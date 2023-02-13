3fe1c4bd79731b239e88a3032099961cd55010d1
Vladimir Lakhtin <99466577+VladimirLakhtin@users.noreply.github.com>
2023-02-13 20:26:46 +0300

    Update create_bot.py
    sps

--- Skill_bot/create_bot.py
+++ Skill_bot/create_bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-TOKEN_BOT = "6010042554:AAG-SSh3RKFh5GUEt_9VU_yT9aCk4tpGGRk"
+TOKEN_BOT = None
 
 storage = MemoryStorage()
 bot = Bot(token=TOKEN_BOT)
